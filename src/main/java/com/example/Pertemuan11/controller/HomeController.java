da8416d7eaffec0854c69ef287badd251cbf2951
nabilasaf <nabilasafitri861@gmail.com>
2026-05-19 14:41:50 +0700

    Menambahkan HomeController

--- src/main/java/com/example/Pertemuan11/controller/HomeController.java
+++ src/main/java/com/example/Pertemuan11/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.example.Pertemuan11.controller;
 
+import com.example.Pertemuan11.model.User;
+import com.example.Pertemuan11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final AuthService authService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        User user = authService.getLoggedInUser();
+
+        model.addAttribute( "user", user);
+        model.addAttribute( "profile", user.getProfile());
+
+        return "home";
+    }
 }
