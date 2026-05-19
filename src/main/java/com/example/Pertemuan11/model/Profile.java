85709715264472dbf78f85cd379b19be4d3af083
nabilasaf <nabilasafitri861@gmail.com>
2026-05-19 14:13:37 +0700

    Menambahkan Profile

--- src/main/java/com/example/Pertemuan11/model/Profile.java
+++ src/main/java/com/example/Pertemuan11/model/Profile.java
@@ -1,4 +1,25 @@
 package com.example.Pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
+
+    @Id
+    private String id;
+    private String name;
+    private String alamat;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
